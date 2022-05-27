17f1906eab92c571430e521dea324420948ed225
Fábio M. Costa <fmc@ufg.br>
2022-05-27 09:57:43 -0300

    Update const.py

--- const.py
+++ const.py
@@ -1,5 +1,5 @@
-CHAT_SERVER_HOST = "172.31.91.151"
+CHAT_SERVER_HOST = "10.128.0.3"
 CHAT_SERVER_PORT = 5001
 
 # IDs de usuários registrados com os respectivos endereços IP e números de porta
-registry = {"Alice":("172.31.82.97",5002), "Bob":("172.31.89.197",5002)}
+registry = {"Alice":("10.128.0.2",5002), "Bob":("10.128.0.4",5002)}
