a83ab106a944b301140f6bb0a39a14c390ef51d7
Fábio M. Costa <fmc@ufg.br>
2023-06-14 17:28:21 -0300

    Changes due to AWS issues with binding with public IP address

--- chatclient.py
+++ chatclient.py
@@ -4,11 +4,13 @@ import pickle
 import threading
 import const #- addresses, port numbers etc. (a rudimentary way to replace a proper naming service)
 
+#
+# This class defines the code run by the receiving thread
 class RecvHandler(threading.Thread):
   def __init__(self, sock):
     threading.Thread.__init__(self)
     self.client_socket = sock
-
+    
   def run(self):
     while True:
         #print('Client receiving handler is ready.')
@@ -21,18 +23,26 @@ class RecvHandler(threading.Thread):
         conn.close()
     return # We need a condition for graceful termination
 
-me = str(sys.argv[1]) # User's name (as registered in the registry. E.g., Alice, Bob, ...)
-client_sock = socket(AF_INET, SOCK_STREAM) # socket for server to connect to this client
-my_ip = const.registry[me][0]   # If using a proper naming service, client should know its
-my_port = const.registry[me][1] # addresses (which it would register in the ns)
-client_sock.bind((my_ip, my_port))
-client_sock.listen(5)
 #
 # Put receiving thread to run
 recv_handler = RecvHandler(client_sock)
 recv_handler.start()
+
 #
-# Handle interactive loop
+# Configure the address of the local user (name given as the first command-line argument)
+try:
+  me = str(sys.argv[1]) # User's name (as registered in the registry. E.g., Alice, Bob, ...)
+except:
+  print('Usage: python3 chatclient.py <Username>')
+  
+client_sock = socket(AF_INET, SOCK_STREAM) # socket for server to connect to this client
+#my_ip = const.registry[me][0]   # If using a proper naming service, client should know its
+my_port = const.registry[me][1] # addresses (which it would register in the ns)
+client_sock.bind(('0.0.0.0', my_port)) # NB: AWS instances don't allow binding to their public IP address!
+client_sock.listen(5)
+
+#
+# Handle loop for user interaction (in the main thread)
 while True:
     server_sock = socket(AF_INET, SOCK_STREAM) # socket to connect to server
     dest = input("ENTER DESTINATION: ")
